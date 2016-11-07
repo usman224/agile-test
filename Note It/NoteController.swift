e1cb46a8eb7460dde0ccc8cbcce87bd98ceddb79
Usman Hussain <UsmanHussain@Usmans-MacBook-Pro.local>
2016-11-07 23:00:50 +0000

    TYK after tests

---
+++ Note It/NoteController.swift
@@ -0,0 +1,35 @@
+//
+//  NoteController.swift
+//  Note It
+//
+//  Created by Usman Hussain on 07/11/2016.
+//  Copyright © 2016 Usman Hussain. All rights reserved.
+//
+
+import UIKit
+
+class NoteController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
